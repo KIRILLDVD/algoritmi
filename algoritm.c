b31d5b6fef997fc55c7ec414ab7247a14d27d9c1
kirill <kolmikov.k@gmail.com>
2024-03-09 10:29:07 +0300

    not rabotaet

--- algoritm.c
+++ algoritm.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
 int main()
 {
-  printf("Hello world\n");
+  char mas[10][21];
+  int i;
+  int  x = 10,y=5;
+  char key;
+  int ox,oy;
+  int ax = 3, ay=4;
+  srand(time(NULL));
+  do
+  {
+  
+  sprintf(mas[0], "####################");
+  for (i = 1; i<9;i++)
+      sprintf(mas[i],"#                  #");
+  sprintf(mas[9], "####################");
+  mas[y][x]= '@';
+  mas[ay][ax]= '*';
+  system("cls");
+  for (i = 0; i<10;i++)
+      printf("%s\n",mas[i]);
+  key = getch();
+  ox =x;
+  oy=y;
+  if (key == 'w') y--;
+  if (key == 's') y++;
+  if (key == 'a') x--;
+  if (key == 'd') x++;
+  if (mas[y][x]=='#'){x=ox;y=oy;}
+  if ((x==ax)&&(y==ay)){
+    ax = rand();
+    ay = rand();
+
+  }
+  }
+  while (key !='e');
   return 0;
 }
